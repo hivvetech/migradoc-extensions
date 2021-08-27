864a94f3a9a009bebff8f9f62127b98cd01184c0
Robert Mansion <robertp.mansion@gmail.com>
2021-08-27 22:06:03 +0800

    Add nuget specs

--- src/MigraDoc.Extensions/Properties/AssemblyInfo.cs
+++ src/MigraDoc.Extensions/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MigraDoc.Extensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MigraDoc.Extensions makes use of MarkdownSharp to convert from Markdown to HTML and the Html Agility Pack to convert from HTML to PDF.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Vertigo Ventures")]
 [assembly: AssemblyProduct("MigraDoc.Extensions")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2021, MIT licensed")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
